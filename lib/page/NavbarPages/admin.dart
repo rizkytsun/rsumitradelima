dd0f866e8d1c86ebfeab04b29ddfc3c2c6aface2
Mohamad Rizky Reynaldy <rizkytsun@gmail.com>
2020-12-24 13:14:24 +0700

    Update hotline.svg, admin.dart, and beranda.dart

--- lib/page/NavbarPages/admin.dart
+++ lib/page/NavbarPages/admin.dart
@@ -11,87 +11,108 @@ class _AdminPageState extends State<AdminPage> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                      child: Image.asset(
+                    'assets/rsmd_logo.png',
+                    width: (MediaQuery.of(context).size.width / 3) - 25.0,
+                  )),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "RSU MITRA DELIMA",
+                      style:
+                          TextStyle(fontSize: 35, fontFamily: 'BauhausReguler'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
               children: <Widget>[
-
-                SafeArea(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Center(child: Image.asset('assets/rsmd_logo.png', width: (MediaQuery.of(context).size.width / 3 ) - 25.0,)),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text("RSU MITRA DELIMA", style: TextStyle(fontSize: 35, fontFamily: 'BauhausReguler'),),
-                      ),
-                    ],
+                Text(
+                  "Halaman Ini Khusus Admin",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black87)),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      size: 30,
+                    ),
+                    hintText: "Masukkan Nomor RM",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    labelText: "Nomor RM",
+                    labelStyle: TextStyle(color: Colors.black87),
                   ),
                 ),
-
-                Column(
-                  children: <Widget>[
-                    Text("Halaman Ini Khusus Admin", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-
-                    SizedBox(height: 20,),
-
-                    TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color : Colors.black87)
-                        ),
-                        prefixIcon: Icon(Icons.person, size: 30,),
-                        hintText: "Masukkan Nomor RM",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        labelText: "Nomor RM",
-                        labelStyle: TextStyle(color: Colors.black87),
-                      ),
-                    ),
-
-                    SizedBox(height: 10,),
-
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color : Colors.black87)
-                        ),
-                        prefixIcon: Icon(Icons.lock, size: 30,),
-                        hintText: "Masukkan NIK KTP",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        labelText: "NIK KTP",
-                        labelStyle: TextStyle(color: Colors.black87),
-                      ),
-                    ),
-
-                    SizedBox(height: 20,),
-
-                    MyButton(
-                      onTap: (){},
-                      text: "Login",
-                      fullRounded: false,
-                      color: Colors.black87,
-                    ),
-                  ],
+                SizedBox(
+                  height: 10,
                 ),
-                
-                Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Copyright ©2020, By :", style: TextStyle(fontSize: 15,),),
-                        Text(" IT RSMD ", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),),
-                        Text("Support ByTech_ID", style: TextStyle(fontSize: 15,),),
-
-                  ],
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black87)),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30,
+                    ),
+                    hintText: "Masukkan NIK KTP",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    labelText: "NIK KTP",
+                    labelStyle: TextStyle(color: Colors.black87),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                MyButton(
+                  onTap: () {},
+                  text: "Login",
+                  fullRounded: false,
+                  color: Colors.black87,
                 ),
               ],
             ),
-          ),
-        );
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Copyright ©2020, By :",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                // Text(" IT RSMD ", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),),
+                Text(
+                  " Support ByTech_ID",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/page/NavbarPages/beranda.dart
+++ lib/page/NavbarPages/beranda.dart
@@ -9,8 +9,7 @@ class BerandaPage extends StatefulWidget {
 }
 
 class _BerandaPageState extends State<BerandaPage> {
-
-static int timelinePage = 1;
+  static int timelinePage = 1;
 
   final PageController controller = PageController(
     initialPage: timelinePage,
@@ -22,8 +21,17 @@ static int timelinePage = 1;
     initialPage: beritaPage,
   );
 
-    _launchURL() async {
-    const url = 'http://rsumitradelima.com:85/webapps/pendaftaran/login.php';
+  _launchWebApp() async {
+    const url = 'http://rsumitradelima.com:85/webapps/epasien';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _launchWA() async {
+    const url = 'https://wa.me/%3C6281340001415%3E';
     if (await canLaunch(url)) {
       await launch(url);
     } else {
@@ -34,106 +42,179 @@ static int timelinePage = 1;
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: <Widget>[
-          // Home AppBar
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
+      children: <Widget>[
+        // Home AppBar
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(35.0))
-            ),
-            child: SafeArea(
-              child: Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Center(child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset('assets/rsmd_logo.png', width: (MediaQuery.of(context).size.width / 5) - 35.0,),
-                    Text(" RSU MITRA DELIMA", style: TextStyle(fontSize: 15, fontFamily: 'BauhausReguler'),)
-                  ],
-                )),
-              ),
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(35.0))),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Center(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/rsmd_logo.png',
+                    width: (MediaQuery.of(context).size.width / 5) - 35.0,
+                  ),
+                  Text(
+                    " RSU MITRA DELIMA",
+                    style:
+                        TextStyle(fontSize: 15, fontFamily: 'BauhausReguler'),
+                  )
+                ],
+              )),
             ),
           ),
+        ),
 
-          // Content Timeline
-          Expanded(
-            child: SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
-                    child: Text('TimeLine', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),),
+        // Content Timeline
+        Expanded(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0, vertical: 15.0),
+                  child: Text(
+                    'TimeLine',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          height: 200.0,
-                          child: PageView.builder(
-                            controller: controller,
-                            onPageChanged: (index) => setState(() => timelinePage = index + 1),
-                            itemCount: 3,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                                child: Image.asset('assets/timeline.png', width: double.infinity,),
-                              );
-                            },
-                            physics: BouncingScrollPhysics(),
-                          ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 200.0,
+                        child: PageView.builder(
+                          controller: controller,
+                          onPageChanged: (index) =>
+                              setState(() => timelinePage = index + 1),
+                          itemCount: 3,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 30.0),
+                              child: Image.asset(
+                                'assets/timeline.png',
+                                width: double.infinity,
+                              ),
+                            );
+                          },
+                          physics: BouncingScrollPhysics(),
                         ),
-                        Padding(padding: EdgeInsets.all(5.0), child: Row(
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Container(
                               width: 10.0,
                               height: 10.0,
                               decoration: BoxDecoration(
-                                color: timelinePage == 1 ? Colors.grey[700] : Colors.grey,
-                                borderRadius: BorderRadius.circular(5)
-                              ),
-                            ),SizedBox(width: 20.0,),
+                                  color: timelinePage == 1
+                                      ? Colors.grey[700]
+                                      : Colors.grey,
+                                  borderRadius: BorderRadius.circular(5)),
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
                             Container(
                               width: 10.0,
                               height: 10.0,
                               decoration: BoxDecoration(
-                                color: timelinePage == 2 ? Colors.grey[700] : Colors.grey,
-                                borderRadius: BorderRadius.circular(5)
-                              ),
-                            ),SizedBox(width: 20.0,),
+                                  color: timelinePage == 2
+                                      ? Colors.grey[700]
+                                      : Colors.grey,
+                                  borderRadius: BorderRadius.circular(5)),
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
                             Container(
                               width: 10.0,
                               height: 10.0,
                               decoration: BoxDecoration(
-                                color: timelinePage == 3 ? Colors.grey[700] : Colors.grey,
-                                borderRadius: BorderRadius.circular(5)
-                              ),
+                                  color: timelinePage == 3
+                                      ? Colors.grey[700]
+                                      : Colors.grey,
+                                  borderRadius: BorderRadius.circular(5)),
                             ),
                           ],
-                        ),)
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
-
-                  SizedBox(height: 20,),
-                
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     InkWell(
-                      onTap: () => _launchURL(),
-                      child: ButtonKotakHome(assetKeSvg: 'assets/home/pendaftaran.svg', text: 'Pendaftaran Online',)
-                    ),
+                        onTap: () => _launchWebApp(),
+                        child: ButtonKotakHome(
+                          assetKeSvg: 'assets/home/pendaftaran.svg',
+                          text: 'Pendaftaran Online',
+                        )),
                     InkWell(
-                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => JadwalDokter()),),
-                      child: ButtonKotakHome(assetKeSvg: 'assets/home/jadwal-dokter.svg', text: 'Jadwal Dokter',)
+                        onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (_) => JadwalDokter()),
+                            ),
+                        child: ButtonKotakHome(
+                          assetKeSvg: 'assets/home/jadwal-dokter.svg',
+                          text: 'Jadwal Dokter',
+                        )),
+                    ButtonKotakHome(
+                      assetKeSvg: 'assets/home/tempat-tidur.svg',
+                      text: 'Tempat Tidur',
                     ),
-                    ButtonKotakHome(assetKeSvg: 'assets/home/tempat-tidur.svg', text: 'Tempat Tidur',),
-                    ButtonKotakHome(assetKeSvg: 'assets/home/bantuan-layanan.svg', text: 'Bantuan Layanan',),
+                    ButtonKotakHome(
+                      assetKeSvg: 'assets/home/bantuan-layanan.svg',
+                      text: 'Bantuan Layanan',
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    InkWell(
+                        onTap: () => _launchWA(),
+                        child: ButtonKotakHome(
+                          assetKeSvg: 'assets/home/hotline.svg',
+                          text: 'Layanan Hotline',
+                        )),
+                    InkWell(
+                        onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (_) => JadwalDokter()),
+                            ),
+                        child: ButtonKotakHome(
+                          assetKeSvg: 'assets/home/jadwal-dokter.svg',
+                          text: 'Jadwal Dokter',
+                        )),
+                    ButtonKotakHome(
+                      assetKeSvg: 'assets/home/tempat-tidur.svg',
+                      text: 'Tempat Tidur',
+                    ),
+                    ButtonKotakHome(
+                      assetKeSvg: 'assets/home/bantuan-layanan.svg',
+                      text: 'Bantuan Layanan',
+                    ),
                   ],
                 )
               ],
@@ -145,11 +226,8 @@ static int timelinePage = 1;
   }
 }
 
-
 class ButtonKotakHome extends StatelessWidget {
-  ButtonKotakHome({
-    this.assetKeSvg, this.text
-  });
+  ButtonKotakHome({this.assetKeSvg, this.text});
 
   final String assetKeSvg;
   final String text;
@@ -158,29 +236,35 @@ class ButtonKotakHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-      color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 4,
-            blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
-          ),
-        ]
-      ),
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 4,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ]),
       width: MediaQuery.of(context).size.width / 4 - 10.0,
       padding: EdgeInsets.all(15),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Container(child: SvgPicture.asset(assetKeSvg, 
+          Container(
+              child: SvgPicture.asset(
+            assetKeSvg,
             width: MediaQuery.of(context).size.width / 4 - 50.0,
-              )),
-
-
-        SizedBox(height: 10,),
-          Flexible(child: Text(text, textAlign: TextAlign.center, style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold ),)),
+          )),
+          SizedBox(
+            height: 10,
+          ),
+          Flexible(
+              child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+          )),
         ],
       ),
     );
